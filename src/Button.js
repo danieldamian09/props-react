fa2d83035d7920c382180727e0ec4852c02b7421
danieldamian09 <humbertorivero9@gmail.com>
2021-05-18 11:44:31 -0300

    modificando-estado-react

---
+++ src/Button.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Button = () => {
+
+  return(
+    <div>
+      <p>Me has Clickeado: 0 </p>
+      <button>Click Me</button>
+    </div>
+  )
+
+}
+
+export default Button;
